03c4e7443ba77ecd346a53b186ab2497c9816d6d
Ivo Oliveira <ivo793@gmail.com>
2024-10-31 21:57:59 -0300

    fazendo requisição na api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/navbar'
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
 
   return (
     <div className='App'>
-
       <Navbar />
-      
-      <h2>My Lib</h2>
       <Outlet></Outlet>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom"
-import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 const Navbar = () =>{
     return(
